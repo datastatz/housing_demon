dd17372dbeb6a2c233f2144a0259ddc998467a59
Ayman Mellouk <barxresearch@mac-1.home>
2025-12-03 18:13:49 +0100

    Progess:

--- app.py
+++ app.py
@@ -12,16 +12,25 @@ async def main():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False, slow_mo=50) 
         page = await browser.new_page()
-        await page.goto("https://plaza.newnewnew.space/?692b3b84947b7")
+        await page.goto("https://www.roofz.eu/availability")
         await page.screenshot(path="example.png")
       
 
-
+ 
         #Clicking on the Login Button
-        await page.get_by_text("Inloggen").first.click()
-        #Wait 30 sec
+        await page.get_by_text("WEIGEREN").first.click() # WORKS
+
+        #Wait 
+        await page.wait_for_timeout(2000)
+        
+        #Open de dropdown
+        await page.locator(".select-control__select").filter(has_text="Newest").click()
+        await page.wait_for_timeout(500)
+       
+        #Selecteer "Newest"
+        await page.locator("li[value='publish-high']").click()
+
         await page.wait_for_timeout(5000)
-        #Fill in username
-        await page.fill("input[name='username']", EMAIL)
+
 
 asyncio.run(main())
