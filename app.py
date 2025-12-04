c05b3a13511acce06c3390fe0991bb362d219e6f
Ayman Mellouk <barxresearch@mac.home>
2025-12-04 11:45:32 +0100

    Increased timeout from 30 to 60 sec

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ async def main():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True) 
         page = await browser.new_page()
-        await page.goto("https://www.roofz.eu/availability")
+        await page.goto("https://www.roofz.eu/availability", timeout=60000, wait_until="domcontentloaded")
         
       
 
